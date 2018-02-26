e1fc47477c7bf4dc7d046d32e3e644e9bd8f8694
taoweicn <twchn@live.com>
2018-02-26 11:30:20 +0800

    feat: separate pop-up modal component

---
+++ src/components/PopupModal/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <transition name="slide-fade">
+    <div class="modal-container" @click.self="back">
+      <div class="modal">
+        <slot></slot>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: 'PopupModal',
+  methods: {
+    back() {
+      this.$router.back();
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped src="./style.scss"></style>
