2c7745316ed54abaac358752dd4811d7811b783d
taoweicn <twchn@live.com>
2018-03-07 23:11:27 +0800

    feat: add Option component

---
+++ src/components/Option/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <transition
+    name="slide"
+  >
+    <li
+      :class="{ [optionsInfo.state ? 'right' : 'wrong']: optionsInfo.selected }"
+      :style="wrongClass"
+    >
+      <slot></slot>
+      <span class="rate">{{ optionsInfo.selected ? rate : '' }}</span>
+    </li>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: 'Option',
+  props: {
+    optionsInfo: Object,
+    rate: {
+      default: '100%',
+      type: String
+    }
+  },
+  computed: {
+    wrongClass() {
+      return {
+        backgroundSize: this.optionsInfo.selected && !this.optionsInfo.state ? `${this.rate} 100%` : ''
+      };
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped src="./style.scss"></style>
