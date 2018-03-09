1f023795e461ed55aa35dc3914ba211a9245243a
taoweicn <twchn@live.com>
2018-03-10 01:31:00 +0800

    feat:  add StatusBar component

---
+++ src/components/StatusBar/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <ul class="status-bar">
+    <li
+      class="number"
+      :class="{ active: n === current, next: n === current + 1 }"
+      v-for="n in count"
+      :key="n"
+    >
+      {{ n }}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'StatusBar',
+  props: {
+    count: {
+      type: Number,
+      required: true
+    },
+    current: {
+      type: Number,
+      required: true
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped src="./style.scss"></style>
