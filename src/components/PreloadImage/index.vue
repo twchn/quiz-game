fe8e40bfe3acc0e1b686c43715958fb546bc45ae
taoweicn <twchn@live.com>
2018-03-10 22:26:08 +0800

    feat: add PreloadImage component

---
+++ src/components/PreloadImage/index.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="loading-mask" v-if="show">
+    <div class="sk-cube-grid">
+      <div class="sk-cube sk-cube1"></div>
+      <div class="sk-cube sk-cube2"></div>
+      <div class="sk-cube sk-cube3"></div>
+      <div class="sk-cube sk-cube4"></div>
+      <div class="sk-cube sk-cube5"></div>
+      <div class="sk-cube sk-cube6"></div>
+      <div class="sk-cube sk-cube7"></div>
+      <div class="sk-cube sk-cube8"></div>
+      <div class="sk-cube sk-cube9"></div>
+    </div>
+    <p>加载中...</p>
+    <p>{{ percentage }}%</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PreloadImage',
+  props: {
+    imgArr: {
+      type: Array,
+      required: true
+    }
+  },
+  data() {
+    return {
+      loadedCount: 0,
+      show: true
+    };
+  },
+  methods: {
+    // 无序加载图片
+    unorderedPreload() {
+      this.imgArr.forEach((imgUrl) => {
+        const imgObj = new Image();
+        imgObj.addEventListener('load', this.imageLoaded);
+        imgObj.addEventListener('error', this.imageLoaded);
+        imgObj.src = imgUrl;
+      });
+    },
+    // 加载完成后执行的回调
+    imageLoaded() {
+      this.loadedCount += 1;
+      if (this.loadedCount >= this.imgArr.length) {
+        this.show = false;
+      }
+    }
+  },
+  computed: {
+    percentage() {
+      return Math.round((this.loadedCount / this.imgArr.length) * 100);
+    }
+  },
+  created() {
+    this.unorderedPreload();
+  }
+};
+</script>
+
+<style lang="scss" scoped src="./style.scss"></style>
