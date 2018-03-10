f7e72b70a819af4457fde5f5b4e192ca5ee2a1e9
taoweicn <twchn@live.com>
2018-03-10 15:23:15 +0800

    chore: remove error notification

--- config/index.js
+++ config/index.js
@@ -17,7 +17,7 @@ module.exports = {
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
-    notifyOnErrors: true,
+    notifyOnErrors: false,
     poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
 
     // Use Eslint Loader?
